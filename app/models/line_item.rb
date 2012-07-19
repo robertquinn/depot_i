bdd30c8ad6e90b91017ed32489c6e8e66352e72b
Rob Quinn <webprorob@gmail.com>
2012-07-18 18:56:29 -0700

    Cart quantity, Cart sum

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,10 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id
+  attr_accessible :cart_id, :product_id, :quantity
   belongs_to :cart
   belongs_to :product
+  
+  def total_price
+    product.price * quantity
+  end
+  
 end
